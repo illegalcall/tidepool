76ca5d85c325666f5b4fb826fc63acec6bcf8bd7
Dhruv Sharma <dhruvcoding67@gmail.com>
2025-11-10 20:42:00 +0530

    init anchor workspace

---
+++ programs/tidepool-clmm/src/lib.rs
@@ -0,0 +1,8 @@
+use anchor_lang::prelude::*;
+
+declare_id!("CLMMhqMRwyFEP5QwZk8MRxSfqjuqSbSEfYdRYag766qU");
+
+#[program]
+pub mod tidepool_clmm {
+    use super::*;
+}
---
+++ programs/tidepool-vault/src/lib.rs
@@ -0,0 +1,8 @@
+use anchor_lang::prelude::*;
+
+declare_id!("TVAULTxVRzBf7JqX77GEQpKTkNLBcEigvbUP5SNBhMn");
+
+#[program]
+pub mod tidepool_vault {
+    use super::*;
+}
